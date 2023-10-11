451979a337393cd3b68303daedcb7cd1b7051b4a
cindy isogie <cindyisogie@gmail.com>
2023-10-11 14:55:51 +0100

    websocket

--- websocket-client-ts/client.ts
+++ websocket-client-ts/client.ts
@@ -10,7 +10,7 @@ ws.on('open', () => {
 });
 
 ws.on('message', (message: string) => {
-    console.log('Received a message from the server: ${message}');
+    console.log(` ${message}`);
 });
 
 ws.on('close', () => {
